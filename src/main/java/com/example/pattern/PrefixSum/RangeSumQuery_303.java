398f90342ba068cbbdeceec63556d0cab85961a1
SiamNurnabi <siam.nurnabi@dsinnovators.com>
2024-09-04 11:52:57 +0600

    Prefix sum doc and RangeSumQuery_303 prob with 1 solution

---
+++ src/main/java/com/example/pattern/PrefixSum/RangeSumQuery_303.java
@@ -0,0 +1,20 @@
+package com.example.pattern.PrefixSum;
+
+public class RangeSumQuery_303 {
+    int[] nums;
+
+    public RangeSumQuery_303(int[] nums) {
+        this.nums = new int[nums.length];
+        this.nums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            this.nums[i] += this.nums[i - 1] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        if (left <= 0) {
+            return nums[right];
+        }
+        return nums[right] - nums[left - 1];
+    }
+}
